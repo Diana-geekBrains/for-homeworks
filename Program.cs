20a84899a1250e4e24d708007faef34fce316590
Diana Valutite <dianavalutite008@gmail.com>
2023-03-12 01:56:42 +0300

    homework3

--- Program.cs
+++ Program.cs
@@ -176,7 +176,75 @@
 
 // 2, 4 -> 16
 
-int NatStep(int num A, int num B )
-{
+//  int NatStep(int numA, int numB )
+//  {
+//   int i=1;
+//   int res=1;
+//  for (i=1; i <= numB; i++)
+//  {
+//      res = res * numA;
+     
+//  }
+//  return res;
+//  }
+//  Console.WriteLine("Input first number:");
+//   int A = Convert.ToInt32(Console.ReadLine());
+//   Console.WriteLine("Input second number:");
+//   int B = Convert.ToInt32(Console.ReadLine());
+//   Console.WriteLine($"{A},{B} -> {NatStep (A, B)}");
 
-}
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+//  int Sum(int n)
+//  {
+//     int number = n;
+//     int res = 0;
+//    while (number > 0)
+//    {
+//     res = res + number % 10;
+//     number = number / 10;
+//    }
+// return res;
+//  }
+// Console.WriteLine("Input number: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"{A}-> {Sum(A)}");
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] CreateRandomArray (int size, int minValue, int maxValue)
+{
+     int[] newArray = new int[size];
+     for(int i = 0; i < size; i++)
+     {
+         newArray[i] = new Random().Next(minValue, maxValue+1);
+     }
+     return newArray;
+ }
+
+ void ShowArray (int[] array)
+ {
+     for(int i = 0; i < array.Length; i++)
+     {
+         Console.Write (array[i] + " ");
+     }
+     Console.WriteLine ();
+ }
+
+ Console.WriteLine ("Input array size:");
+ int size = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine ("Input minimal value of array element:");
+ int minValue = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine ("Input maximal value of array element:");
+ int maxValue = Convert.ToInt32(Console.ReadLine());
+ ShowArray(CreateRandomArray(size, minValue, maxValue));
