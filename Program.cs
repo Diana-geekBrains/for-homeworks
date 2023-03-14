ba0d5ddd2fac2a080adca467b92002bdfba8312f
Diana Valutite <dianavalutite008@gmail.com>
2023-03-15 01:10:26 +0300

    add

--- Program.cs
+++ Program.cs
@@ -222,33 +222,33 @@
 
 // 6, 1, 33 -> [6, 1, 33]
 
-int[] CreateRandomArray (int size, int minValue, int maxValue)
-{
-     int[] newArray = new int[size];
-     for(int i = 0; i < size; i++)
-     {
-         newArray[i] = new Random().Next(minValue, maxValue+1);
-     }
-     return newArray;
- }
+// int[] CreateRandomArray (int size, int minValue, int maxValue)
+// {
+//      int[] newArray = new int[size];
+//      for(int i = 0; i < size; i++)
+//      {
+//          newArray[i] = new Random().Next(minValue, maxValue+1);
+//      }
+//      return newArray;
+//  }
 
- void ShowArray (int[] array)
- {
-    Console.Write ("[");
-     for(int i = 0; i < array.Length; i++)
-     {
+//  void ShowArray (int[] array)
+//  {
+//     Console.Write ("[");
+//      for(int i = 0; i < array.Length; i++)
+//      {
         
-         Console.Write (array[i]  + " ");
+//          Console.Write (array[i]  + " ");
          
-     }
-     Console.Write ("]");
-     Console.WriteLine ();
- }
+//      }
+//      Console.Write ("]");
+//      Console.WriteLine ();
+//  }
 
- Console.WriteLine ("Input array size:");
- int size = Convert.ToInt32(Console.ReadLine());
- Console.WriteLine ("Input minimal value of array element:");
- int minValue = Convert.ToInt32(Console.ReadLine());
- Console.WriteLine ("Input maximal value of array element:");
- int maxValue = Convert.ToInt32(Console.ReadLine());
- ShowArray(CreateRandomArray(size, minValue, maxValue));
+//  Console.WriteLine ("Input array size:");
+//  int size = Convert.ToInt32(Console.ReadLine());
+//  Console.WriteLine ("Input minimal value of array element:");
+//  int minValue = Convert.ToInt32(Console.ReadLine());
+//  Console.WriteLine ("Input maximal value of array element:");
+//  int maxValue = Convert.ToInt32(Console.ReadLine());
+//  ShowArray(CreateRandomArray(size, minValue, maxValue));
